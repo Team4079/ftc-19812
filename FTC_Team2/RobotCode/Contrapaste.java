cf9f77f2049aae82be6672c4e8f20f45f895f5b7
Alex <hifiv2020@gmail.com>
2024-09-26 15:50:35 -0700

    Contrapaste File

--- FTC_Team2/RobotCode/Contrapaste.java
+++ FTC_Team2/RobotCode/Contrapaste.java
@@ -10,18 +10,13 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 @TeleOp
 
 public class ArmCode extends LinearOpMode {
-    private ElapsedTime runtime = new ElapsedTime();
-    private DcMotor armDrive;
+    
     
     @Override
     public void runOpMode(){
-        armDrive = hardwareMap.get(DcMotor.class, "grabbyMot");
-        armDrive.setDirection(DcMotor.Direction.FORWARD);
         
-        waitForStart();
-        runtime.reset();
         while(opModeIsActive()){
-            armDrive.setPower(0.1);
+
         }
     }
 }
