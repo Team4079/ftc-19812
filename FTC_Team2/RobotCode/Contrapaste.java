d088a80f15138e609cba1494697761314e7bc0de
Alex <hifiv2020@gmail.com>
2024-09-26 16:28:00 -0700

    Contrapaste Arm Code

--- FTC_Team2/RobotCode/Contrapaste.java
+++ FTC_Team2/RobotCode/Contrapaste.java
@@ -10,12 +10,50 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 @TeleOp
 
 public class ArmCode extends LinearOpMode {
-    
+    private ElapsedTime runtime = new ElapsedTimed();
+    private int Armstate = 0;
+    private double armCooldown = 0;
+
+    jerkoffarm= hardwareMap.get(DcMotor.class, “jerk_off_arm”);
+    jerkoffarm.setPower(0.0);
+
+    private int ArmstateTwo =0;
+    private double armCooldownTwo = 0;
+
+    weakarm= hardwareMap.get(DcMotor.class, “weak_arm”);
+    weakarm.setPower(0.0);
     
     @Override
     public void runOpMode(){
         
-        while(opModeIsActive()){
+while(opModeIsActive()){
+    if(gamepad1_button_b == true && (runtime - ArmCooldown)>==1){
+    Armstate++
+    if (Armstate == 1){
+        jerkoffarm.setpower(1.0);
+    }else if (Armstate == 2){
+        jerkoffarm.setpower(-1.0);
+    }else if (Armstate == 3){
+        jerkoffarm.setpower(0.0)
+    }
+}
+If (Armstate >4){
+Armstate = 1;
+}ArmCooldown = runtime;
+
+    if(gamepad1_button_a == true && (runtime - ArmCooldownTwo)>==1){
+    ArmstateTwo++
+    if (ArmstateTwo == 1){
+        weakarm.setpower(1.0);
+    }else if (ArmstateTwo == 2){
+        weakarm.setpower(-1.0);
+    }
+}
+If (ArmstateTwo >3){
+ArmstateTwo = 1;
+}ArmCooldownTwo = runtime;
+
+
 
         }
     }
