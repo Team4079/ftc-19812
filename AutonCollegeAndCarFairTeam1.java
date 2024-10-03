60ea64717b92c8612f9a6f0af9d1ca2f4b863b55
Justin Nguyen <jnthemusicman2010@gmail.com>
2024-10-03 16:01:29 -0700

    Copied stuff from rishi. Should be fine.

---
+++ AutonCollegeAndCarFairTeam1.java
@@ -0,0 +1,55 @@
+//justin was here
+package RobotCode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@Autonomous(name="Basic: Omni Linear OpMode", group="Linear Opmode")
+
+public class MechanumAutonomous extends LinearOpMode {
+    private final double wheelCircumference = 75*3.14;
+    private final double gearReduction = 3.61*5.23;
+    private final double counts = 28.0;
+    
+    private final double rev = counts*gearReduction;
+    private final int revPerMM = (int)rev/(int)wheelCircumference;
+    private final double inches = revPerMM*25.4;
+
+    // Declare OpMode members for each of the 4 motors.
+    private ElapsedTime runtime = new ElapsedTime();
+    private DcMotorEx leftFrontDrive = null;
+    private DcMotorEx leftBackDrive = null;
+    private DcMotorEx rightFrontDrive = null;
+    private DcMotorEx rightBackDrive = null;
+
+    @Override
+    public void runOpMode() {
+         // Initialize the hardware variables. Note that the strings used here must correspond
+        // to the names assigned during the robot configuration step on the DS or RC devices.
+        leftFrontDrive  = hardwareMap.get(DcMotorEx.class, "fLeft");
+        leftBackDrive  = hardwareMap.get(DcMotorEx.class, "bLeft");
+        rightFrontDrive = hardwareMap.get(DcMotorEx.class, "fRight");
+        rightBackDrive = hardwareMap.get(DcMotorEx.class, "bRight");
+
+        leftFrontDrive.setDirection(DcMotorEx.Direction.REVERSE);
+        leftBackDrive.setDirection(DcMotorEx.Direction.REVERSE);
+        rightFrontDrive.setDirection(DcMotorEx.Direction.REVERSE);
+        rightBackDrive.setDirection(DcMotorEx.Direction.FORWARD);
+
+         // Wait for the game to start (driver presses PLAY)
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+        int count=0;
+        waitForStart();
+        runtime.reset();
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()&&count==0) { 
+            // Show the elapsed game time and wheel power.
+            count++;
+        }
