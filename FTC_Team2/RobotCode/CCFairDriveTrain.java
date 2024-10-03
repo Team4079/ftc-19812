0301e6d98bc97ad3453cb34ca473126d3b61c480
ReallyOutOfContext <1073930@student.auhsd.us>
2024-10-03 15:48:21 -0700

    Added Imports

---
+++ FTC_Team2/RobotCode/CCFairDriveTrain.java
@@ -0,0 +1,8 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
