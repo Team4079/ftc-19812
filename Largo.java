23352c96d250d7868ef61d7377d09358b8f98daf
Aidan Dwardee <aidan.duarte25@gmail.com>
2024-10-08 15:55:22 -0700

    Added the private variables.

--- Largo.java
+++ Largo.java
@@ -16,3 +16,41 @@ import org.firstinspires.ftc.vision.VisionPortal;
 import org.firstinspires.ftc.vision.tfod.TfodProcessor;
 
 import java.util.List;
+
+@Autonomous(name="Largo practice Auto", group="Linear Opmode")
+public class SutraAutoCloseBlueFlowchart extends LinearOpMode {
+    private final double wheelCircumference = 75*3.14;
+    private final double gearReduction = 3.61*5.23;
+    private final double counts = 28.0;
+    
+    private final double rev = counts*gearReduction;
+    private final int revPerMM = (int)rev/(int)wheelCircumference;
+    private final double inches = revPerMM*25.4;
+
+    // Declare OpMode members for each of the 4 motors.
+    private ElapsedTime runtime = new ElapsedTime();
+    private DcMotorEx leftFrontDrive;
+    private DcMotorEx leftBackDrive;
+    private DcMotorEx rightFrontDrive;
+    private DcMotorEx rightBackDrive;
+    private CRServo leftServo;
+    private CRServo rightServo;
+    private DcMotor leftArm;
+    private DcMotor rightArm;
+    private DcMotor RSlideMotor; //extend
+    private DcMotor LSlideMotor; //extend
+    private String testCase = "";
+    
+    private static final boolean USE_WEBCAM = true;  // true for webcam, false for phone camera
+
+    // TFOD_MODEL_ASSET points to a model file stored in the project Asset location,
+    // this is only used for Android Studio when using models in Assets.
+    private static final String TFOD_MODEL_ASSET = "MyModelStoredAsAsset.tflite";
+    // TFOD_MODEL_FILE points to a model file stored onboard the Robot Controller's storage,
+    // this is used when uploading models directly to the RC using the model upload interface.
+    private static final String TFOD_MODEL_FILE = "/sdcard/FIRST/tflitemodels/ROLL-TAPE-Biased.tflite";
+    // Define the labels recognized in the model for TFOD (must be in training order!)
+    private static final String[] LABELS = {
+      "blue",
+      "red",
+    };
