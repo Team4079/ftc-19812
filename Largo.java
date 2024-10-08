8895048f5f381689760d5aa7633e8394f9af201d
Nebula <navkaran15121@gmail.com>
2024-10-08 16:08:40 -0700

    Largo Update

--- Largo.java
+++ Largo.java
@@ -18,7 +18,7 @@ import org.firstinspires.ftc.vision.tfod.TfodProcessor;
 import java.util.List;
 
 @Autonomous(name="Largo practice Auto", group="Linear Opmode")
-public class SutraAutoCloseBlueFlowchart extends LinearOpMode {
+public class Largo extends LinearOpMode {
     private final double wheelCircumference = 75*3.14;
     private final double gearReduction = 3.61*5.23;
     private final double counts = 28.0;
@@ -50,7 +50,51 @@ public class SutraAutoCloseBlueFlowchart extends LinearOpMode {
     // this is used when uploading models directly to the RC using the model upload interface.
     private static final String TFOD_MODEL_FILE = "/sdcard/FIRST/tflitemodels/ROLL-TAPE-Biased.tflite";
     // Define the labels recognized in the model for TFOD (must be in training order!)
+
+    driveEncoders(50);
+    leftEncoders(50);
+    bottomLeft(70);
     private static final String[] LABELS = {
       "blue",
       "red",
-    };
+    }
+public void drive()
+    {
+        leftFrontDrive.setPower(1.0);
+        rightFrontDrive.setPower(1.0);
+        leftBackDrive.setPower(1.0);
+        rightBackDrive.setPower(1.0);
+        //input(0.4, 0.4, 0.4, 0.4);
+    }
+    public void topRight()
+    {
+        input(0.4, 0, 0, 0.4);
+    }
+    public void right()
+    {
+        input(0.4, -0.4, -0.4, 0.4);
+    }
+    public void bottomRight()
+    {
+        input(0, -0.4, -0.4, 0);
+    }
+    public void back()
+    {
+        input(-0.4, -0.4, -0.4, -0.4);
+    }
+    public void bottomLeft()
+    {
+        input(-0.4, 0, 0, -0.4);
+    }
+    public void left()
+    {
+        input(-0.4, 0.4, 0.4, -0.4);   
+    }
+    public void topLeft()
+    {
+        input(0, 0.4, 0.4, 0);
+    }
+    public void stop(double time)
+    {
+        input(0, 0, 0, 0);
+    }
