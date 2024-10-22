65eb0e30ab8e1671d1a4316e743cabf38d4f3114
grimaceshakeicespicebussingyatohio <lamtruongnguyen888@gmail.com>
2024-10-22 15:43:54 -0700

    autolongspecimen fixes

--- FTC_Team2/RobotCode/AutoLongSpecimen.java
+++ FTC_Team2/RobotCode/AutoLongSpecimen.java
@@ -1,6 +1,8 @@
-package org.firstinspires.ftc.teamcode;
+//justin was here
+package RobotCode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -8,7 +10,6 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 @Autonomous(name="Basic: Omni Linear OpMode", group="Linear Opmode")
-
 
 public class MechanumAutonomous extends LinearOpMode {
     private final double wheelCircumference = 75*3.14;
@@ -21,26 +22,26 @@ public class MechanumAutonomous extends LinearOpMode {
 
     // Declare OpMode members for each of the 4 motors.
     private ElapsedTime runtime = new ElapsedTime();
-    private DcMotor leftFrontDrive = null;
-    private DcMotor leftBackDrive = null;
-    private DcMotor rightFrontDrive = null;
-    private DcMotor rightBackDrive = null;
+    private DcMotorEx leftFrontDrive = null;
+    private DcMotorEx leftBackDrive = null;
+    private DcMotorEx rightFrontDrive = null;
+    private DcMotorEx rightBackDrive = null;
 
     @Override
     public void runOpMode() {
-        
-        // Initialize the hardware variables. Note that the strings used here must correspond
+         // Initialize the hardware variables. Note that the strings used here must correspond
         // to the names assigned during the robot configuration step on the DS or RC devices.
-        leftFrontDrive  = hardwareMap.get(DcMotor.class, "fLeft");
-        leftBackDrive  = hardwareMap.get(DcMotor.class, "bLeft");
-        rightFrontDrive = hardwareMap.get(DcMotor.class, "fRight");
-        rightBackDrive = hardwareMap.get(DcMotor.class, "bRight");
+        leftFrontDrive  = hardwareMap.get(DcMotorEx.class, "fLeft");
+        leftBackDrive  = hardwareMap.get(DcMotorEx.class, "bLeft");
+        rightFrontDrive = hardwareMap.get(DcMotorEx.class, "fRight");
+        rightBackDrive = hardwareMap.get(DcMotorEx.class, "bRight");
 
-        leftFrontDrive.setDirection(DcMotor.Direction.REVERSE);
-        leftBackDrive.setDirection(DcMotor.Direction.REVERSE);
-        rightFrontDrive.setDirection(DcMotor.Direction.REVERSE);
-        rightBackDrive.setDirection(DcMotor.Direction.FORWARD);
-        // Wait for the game to start (driver presses PLAY)
+        leftFrontDrive.setDirection(DcMotorEx.Direction.REVERSE);
+        leftBackDrive.setDirection(DcMotorEx.Direction.REVERSE);
+        rightFrontDrive.setDirection(DcMotorEx.Direction.REVERSE);
+        rightBackDrive.setDirection(DcMotorEx.Direction.FORWARD);
+
+         // Wait for the game to start (driver presses PLAY)
         telemetry.addData("Status", "Initialized");
         telemetry.update();
         int count=0;
@@ -48,64 +49,17 @@ public class MechanumAutonomous extends LinearOpMode {
         runtime.reset();
 
         // run until the end of the match (driver presses STOP)
-        while (opModeIsActive()&&count==0) {
-            double max;
-
-            // POV Mode uses left joystick to go forward & strafe, and right joystick to rotate.
-            double speed   = 0.0;  // Note: pushing stick forward gives negative value
-            double strafe =  0.0;
-            double turn = 0.0;
-
-            // Combine the joystick requests for each axis-motion to determine each wheel's power.
-            // Set up a variable for each drive wheel to save the power level for telemetry.
-            double leftFrontPower  = speed + turn + strafe;
-            double rightFrontPower = speed - turn - strafe;
-            double leftBackPower   = speed - turn + strafe;
-            double rightBackPower  = speed + turn - strafe;
-
-            // Normalize the values so no wheel power exceeds 100%
-            // This ensures that the robot maintains the desired motion.
-            max = Math.max(Math.abs(leftFrontPower), Math.abs(rightFrontPower));
-            max = Math.max(max, Math.abs(leftBackPower));
-            max = Math.max(max, Math.abs(rightBackPower));
-
-            if (max > 1.0) {
-                leftFrontPower  /= max;
-                rightFrontPower /= max;
-                leftBackPower   /= max;
-                rightBackPower  /= max;
-            }
+        while (opModeIsActive()&&count==0) { 
+            driveEncoders(1830);
+            turnRight(180);
+            driveEncoders(610);
+            turnLeft(90);
+            driveencoders(1830);
             
-            // This is test code:
-            //
-            // Uncomment the following code to test your motor directions.
-            // Each button should make the corresponding motor run FORWARD.
-            //   1) First get all the motors to take to correct positions on the robot
-            //      by adjusting your Robot Configuration if necessary.
-            //   2) Then make sure they run in the correct direction by modifying the
-            //      the setDirection() calls above.
-            // Once the correct motors move in the correct direction re-comment this code.
-
-            /*
-            leftFrontPower  = gamepad1.x ? 1.0 : 0.0;  // X gamepad
-            leftBackPower   = gamepad1.a ? 1.0 : 0.0;  // A gamepad
-            rightFrontPower = gamepad1.y ? 1.0 : 0.0;  // Y gamepad
-            rightBackPower  = gamepad1.b ? 1.0 : 0.0;  // B gamepad
-            */
-
-            // Send calculated power to wheels
-            //forward
-            
-            encoders();
-            stop(1000);
             // Show the elapsed game time and wheel power.
-            telemetry.addData("Status", "Run Time: " + runtime.toString());
-            telemetry.addData("Front left/Right", "%4.2f, %4.2f", leftFrontPower, rightFrontPower);
-            telemetry.addData("Back  left/Right", "%4.2f, %4.2f", leftBackPower, rightBackPower);
-            telemetry.update();
             count++;
         }
-    }
+         }
     public void input(double leftFront, double rightFront, double leftBack, double rightBack)
     {
         leftFrontDrive.setPower(leftFront);
@@ -114,47 +68,85 @@ public class MechanumAutonomous extends LinearOpMode {
         rightBackDrive.setPower(rightBack);
         sleep(500);
     }
-    public void encoders()
+    public void turnLeft(int angle)
     {
-        int target=50*revPerMM;
+        int convert=revPerMM*angle*(38/10)+(angle*12/10);
+        encoders(-convert, convert, -convert, convert);
+    }
+    public void turnRight(int angle)
+    {
+        int convert=revPerMM*angle*(38/10)+(angle*12/10);
+        encoders(convert, -convert, convert, -convert);
+    }
+    public void driveEncoders(int target)
+    {
+        encoders(target, target, target, target);
+    }
+    public void backEncoders(int target)
+    {
+        encoders(-target, -target, -target, -target);
+    }
+    public void rightEncoders(int target)
+    {
+        encoders(target, -target, -target, target);
+    }
+    public void leftEncoders(int target)
+    {
+        encoders(-target, target, target, -target);
+    }
+    public void leftTopDiagonal(int target)
+    {
+        encoders(0, target, target, 0);
+    }
+    public void rightTopDiagonal(int target)
+    {
+        encoders(target, 0, 0, target);
+    }
+    public void leftBottomDiagonal(int target)
+    {
+        encoders(-target, 0, 0, -target);
+    }
+    public void rightBottomDiagonal(int target)
+    {
+        encoders(0, -target, -target, 0);
+    }
+    public void encoders(int leftFront, int rightFront, int leftBack, int rightBack)
+    {
         leftFrontDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         rightFrontDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         leftBackDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         rightBackDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         
-        leftFrontDrive.setTargetPosition(target);
-        rightFrontDrive.setTargetPosition(target);
-        leftBackDrive.setTargetPosition(target);
-        rightBackDrive.setTargetPosition(target);
+        leftFrontDrive.setTargetPosition(leftFront*revPerMM);
+        rightFrontDrive.setTargetPosition(rightFront*revPerMM);
+        leftBackDrive.setTargetPosition(leftBack*revPerMM);
+        rightBackDrive.setTargetPosition(rightBack*revPerMM);
         
         leftFrontDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         rightFrontDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         leftBackDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         rightBackDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         
-        
-        leftFrontDrive.setPower(1.0);
-        rightFrontDrive.setPower(1.0);
-        leftBackDrive.setPower(1.0);
-        rightBackDrive.setPower(1.0);
-        while (opModeIsActive()&&leftFrontDrive.isBusy()&&rightFrontDrive.isBusy()&&leftBackDrive.isBusy()&&rightBackDrive.isBusy())
+        double power=0.2;
+        leftFrontDrive.setVelocity(1500);
+        rightFrontDrive.setVelocity(1500);
+        leftBackDrive.setVelocity(1500);
+        rightBackDrive.setVelocity(1500);
+        while (opModeIsActive()&&leftFrontDrive.isBusy()||rightFrontDrive.isBusy()||leftBackDrive.isBusy()||rightBackDrive.isBusy())
         {
-            
         }
         leftFrontDrive.setPower(0);
         rightFrontDrive.setPower(0);
         leftBackDrive.setPower(0);
         rightBackDrive.setPower(0);
-
-        driveencoders(1830);
-        turnright(180);
-        driveencoders(610);
-        turnleft(90);
-        driveencoders(1830);
     }
     public void drive()
     {
-        input(0.4, 0.4, 0.4, 0.4);
+        leftFrontDrive.setPower(1.0);
+        rightFrontDrive.setPower(1.0);
+        leftBackDrive.setPower(1.0);
+        rightBackDrive.setPower(1.0);
+        //input(0.4, 0.4, 0.4, 0.4);
     }
     public void topRight()
     {
@@ -189,4 +181,7 @@ public class MechanumAutonomous extends LinearOpMode {
         input(0, 0, 0, 0);
     }
 }
-//rename
+
+        
+    
+   
